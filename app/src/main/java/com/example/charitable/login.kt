ee38c21f154167fcb591105914382c0a23a9c9ea
Pranamya-19 <pranu11v@gmail.com>
2022-02-04 15:25:35 +0530

    dont update anything for now

--- app/src/main/java/com/example/charitable/login.kt
+++ app/src/main/java/com/example/charitable/login.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.login.*
 
-class login : AppCompatActivity() {
+open class login : BaseActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class login : AppCompatActivity() {
             logInLayout.visibility= View.GONE
             logIn.setTextColor(resources.getColor(R.color.bluecolor,null))
         }
+       Signup.setOnClickListener{
+           registerUser()
+       }
         logIn.setOnClickListener {
             Signup.background = null
             Signup.setTextColor(resources.getColor(R.color.teal_200,null))
@@ -31,8 +36,37 @@ class login : AppCompatActivity() {
         }
         signIn.setOnClickListener {
             startActivity(Intent(this@login,splash2::class.java))
-        }
-        }
 
+        }
+        }
+    private fun registerUser(){
+        val email:String= eMail2.text.toString().trim{ it<=' '}
+        val password:String= Passwords2.text.toString().trim{ it<=' '}
+        if(validateForm(email, password)){
+            Toast.makeText(
+                this@login,
+                "Now we can register a new user",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
     }
+
+    private fun validateForm(email:String, password:String):Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Enter your Email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Enter your Password")
+                false
+            }
+            else->{
+                true
+            }
+        }
+        }
+    }
+
 
---
+++ app/src/test/java/com/example/charitable/FirebaseAuth.kt
@@ -0,0 +1,5 @@
+package com.example.charitable
+
+class FirebaseAuth {
+
+}
