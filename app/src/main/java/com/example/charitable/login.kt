9ef33cd25977e64ff88069fe43deabc498f5323b
Pranamya-19 <pranu11v@gmail.com>
2022-02-05 17:57:35 +0530

    7.2

--- app/src/main/java/com/example/charitable/login.kt
+++ app/src/main/java/com/example/charitable/login.kt
@@ -1,19 +1,29 @@
 package com.example.charitable
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.login.*
 
 class login : BaseActivity() {
+
+
+    private lateinit var auth: FirebaseAuth
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        auth = FirebaseAuth.getInstance()
         Signup.setOnClickListener {
             Signup.background = resources.getDrawable(R.drawable.switch_trcks,null)
             Signup.setTextColor(resources.getColor(R.color.textColor,null))
@@ -38,9 +48,12 @@ class login : BaseActivity() {
             startActivity(Intent(this@login, splash2::class.java))
             btnsignup.setOnClickListener {
                 startActivity(Intent(this@login, splash2::class.java))
-            }}}}
-/*
-// TODO "add signUp button here"
+            }
+            signInRegisteredUser()
+
+        }
+
+
        btnsignup.setOnClickListener{
             registerUser()
         }
@@ -49,21 +62,39 @@ class login : BaseActivity() {
 
     private fun registerUser() {
         // Here we get the text from editText and trim the space
+        val name: String = user_name.text.toString().trim { it <= ' ' }
         val email: String = eMail2.text.toString().trim { it <= ' ' }
         val password: String = Passwords2.text.toString().trim { it <= ' ' }
 
-        if (validateForm(email, password)) {
-            Toast.makeText(
-                this@login, "now we can register a new user",
-//                task.exception!!.message,
-                Toast.LENGTH_SHORT
-            ).show()
+        if (validateForm(name, email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+                hideProgressDialog()
+                if (task.isSuccessful) {
+                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                    val registeredEmail = firebaseUser.email!!
+                    Toast.makeText(
+                        this,
+                        "$name you have successfully registered the email address $registeredEmail. ",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+                } else {
+                    Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                }
+
+            }
         }
     }
 
 
-    private fun validateForm(email: String, password: String): Boolean {
+    private fun validateForm(name : String, email: String, password: String): Boolean {
         return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter name.")
+                false
+            }
             TextUtils.isEmpty(email) -> {
                 showErrorSnackBar("Please enter email.")
                 false
@@ -76,7 +107,49 @@ class login : BaseActivity() {
                 true
             }
         }
+
+    }
+     private fun signInRegisteredUser(){
+         val email: String = eMail.text.toString().trim { it <= ' ' }
+         val password: String = Passwords.text.toString().trim { it <= ' ' }
+
+         if (validateForm2(email, password)) {
+             showProgressDialog(resources.getString(R.string.please_wait))
+             auth.signInWithEmailAndPassword(email, password)
+                 .addOnCompleteListener(this) { task ->
+                     hideProgressDialog()
+                     if (task.isSuccessful) {
+                         // Sign in success, update UI with the signed-in user's information
+                         Log.d("sign in", "signInWithEmail:success")
+                         val user = auth.currentUser
+                         startActivity(Intent(this, MainActivity::class.java))
+                     } else {
+                         // If sign in fails, display a message to the user.
+                         Log.w("sign in", "signInWithEmail:failure", task.exception)
+                         Toast.makeText(baseContext, "Authentication failed.",
+                             Toast.LENGTH_SHORT).show()
+
+                     }
+                 }
+         }
+     }
+
+    private fun validateForm2(email: String, password: String): Boolean {
+        return when {
+
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter email.")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter password.")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+
+    }
     }
 
-    }}
-*/
