824a358e8716a3c9079fcc3f29e2f1789c1b877e
shivanivikhar <shivanivikhar3775@gmail.com>
2022-03-02 18:12:57 +0530

    ui final update

---
+++ app/src/main/java/com/example/charitable/rough.kt
@@ -0,0 +1,11 @@
+package com.example.charitable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class rough : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rough)
+    }
+}
