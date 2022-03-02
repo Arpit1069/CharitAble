aaba4094e086c09b954dbdb0027eb6ca63f32534
Pranamya-19 <pranu11v@gmail.com>
2022-03-02 20:16:56 +0530

    update

--- app/src/main/java/com/example/charitable/ngo_one.kt
+++ app/src/main/java/com/example/charitable/ngo_one.kt
@@ -3,7 +3,7 @@ package com.example.charitable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ngo_one : AppCompatActivity() {
+class ngo_one : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ngo_one)
