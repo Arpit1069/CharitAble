ed70a89603c12ce650be71b50dc7d35b7f9d6e2b
shivanivikhar <shivanivikhar3775@gmail.com>
2022-03-03 22:52:31 +0530

    splash4 update

---
+++ app/src/main/java/com/example/charitable/splash4.kt
@@ -0,0 +1,18 @@
+package com.example.charitable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash4)
+//        val time : Long = 2500
+//        Handler().postDelayed(Runnable {
+//        val intent = Intent(this@splash3,::class.java)
+//        startActivity(intent)
+//        finish()
+//    },time)
+
+    }
+}
