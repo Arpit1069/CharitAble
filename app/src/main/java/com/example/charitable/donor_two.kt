5997aad2c5c331e8fac4247030d2e9f6d7665222
knightdrago <97534611+knightdrago@users.noreply.github.com>
2022-03-05 18:19:18 +0530

    chiku's update

--- app/src/main/java/com/example/charitable/donor_two.kt
+++ app/src/main/java/com/example/charitable/donor_two.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_donor_two.*
 class donor_two : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        addFragment(HomeFragment_donor.newInstance())
         setContentView(R.layout.activity_donor_two)
         donor_bottom_nav.add(MeowBottomNavigation.Model(0,R.drawable.ic_home_))
         donor_bottom_nav.add(MeowBottomNavigation.Model(1,R.drawable.ic_history))
@@ -39,7 +40,7 @@ class donor_two : BaseActivity() {
         val fragmentTransition = supportFragmentManager.beginTransaction()
         fragmentTransition.replace(R.id.FragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
     }
-    private fun addFragment(fragment: Fragment) {
+    private fun addFragment(fragment:Fragment) {
         val fragmentTransition = supportFragmentManager.beginTransaction()
         fragmentTransition.add(R.id.FragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
 
