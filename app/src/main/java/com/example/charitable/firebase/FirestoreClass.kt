85ae4a304461faf804c9f99b8f42ee4e535a587d
Ayushvidh <ayushvidhale099@gmail.com>
2022-03-07 16:12:32 +0530

    meow meow forever

--- app/src/main/java/com/example/charitable/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/charitable/firebase/FirestoreClass.kt
@@ -6,7 +6,6 @@ import android.widget.Toast
 import com.example.charitable.donor_one
 import com.example.charitable.login
 import com.example.charitable.models.User
-import com.example.charitable.restaurant_one
 import com.example.charitable.utils.Constants
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -16,36 +15,36 @@ import com.google.firebase.firestore.SetOptions
 class FirestoreClass {
     private val mFireStore = FirebaseFirestore.getInstance()
     fun registerUser(activity: login, userInfo: com.example.charitable.models.User){
-    mFireStore.collection(Constants.USERS)
-        .document(getCurrentUserId()).set(userInfo, SetOptions.merge())
-        .addOnSuccessListener {
-            activity.userRegisteredSuccess()
-        }
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId()).set(userInfo, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userRegisteredSuccess()
+            }
     }
 
-   fun updateUserProfileData(activity: restaurant_one, userHashMap: HashMap<String,Any>){
-       mFireStore.collection(Constants.USERS)
-           .document(getCurrentUserId())
-           .update(userHashMap)
-           .addOnSuccessListener {
-               Log.i(activity.javaClass.simpleName,"Profile data updated successfully")
-               Toast.makeText(activity,"Profile updated successfully",Toast.LENGTH_SHORT).show()
-               //activity.profileUpdateSuccess()
-           }.addOnFailureListener{
-               e ->
-               activity.hideProgressDialog()
-               Log.e(activity.javaClass.simpleName,"Error while creating aboard",e)
-               Toast.makeText(activity,"Error while updating profile",Toast.LENGTH_SHORT).show()
-           }
+    fun updateUserProfileData(activity: donor_one, userHashMap: HashMap<String,Any>){
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .update(userHashMap)
+            .addOnSuccessListener {
+                Log.i(activity.javaClass.simpleName,"Profile data updated successfully")
+                Toast.makeText(activity,"Profile updated successfully",Toast.LENGTH_SHORT).show()
+                activity.profileUpdateSuccess()
+            }.addOnFailureListener{
+                    e ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName,"Error while creating aboard",e)
+                Toast.makeText(activity,"Error while updating profile",Toast.LENGTH_SHORT).show()
+            }
 
-   }
+    }
 
     fun loadUserData(activity: Activity){
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserId()).get()
             .addOnSuccessListener {document->
 
-             val loggedInUser = document.toObject(User::class.java)!!
+                val loggedInUser = document.toObject(User::class.java)!!
 
                 when(activity){
                     is login -> {
@@ -54,9 +53,9 @@ class FirestoreClass {
                     is donor_one ->{
                         activity.setUserDataInUI(loggedInUser)
                     }
-                // is nav_drawer ->{
-                   //  activity.updateNavigationUserDetails(loggedInUser)
-                   // }
+                    // is nav_drawer ->{
+                    //  activity.updateNavigationUserDetails(loggedInUser)
+                    // }
                 }
 
 
@@ -65,7 +64,7 @@ class FirestoreClass {
 
     fun getCurrentUserId(): String{
         var currentUser = FirebaseAuth.getInstance().currentUser
-       var currentUserID = ""
+        var currentUserID = ""
         if(currentUser!=null){
             currentUserID = currentUser.uid
         }
--- app/src/main/java/com/example/charitable/splash2.kt
+++ app/src/main/java/com/example/charitable/splash2.kt
@@ -12,11 +12,9 @@ class splash2 : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash2)
         val time : Long = 2500
-        Handler().postDelayed(Runnable
-        {
-
-         val intent = Intent(this@splash2,donor_two::class.java)
-            startActivity(intent)
+        Handler().postDelayed(Runnable {
+//         val intent = Intent(this@splash2,donor_two::class.java)
+            startActivity(Intent(this@splash2,donor_two::class.java))
             finish()
         },time)
     }
