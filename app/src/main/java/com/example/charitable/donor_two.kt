7c05a1df675985c7b415ae38ed2b249c9b2a8226
Ayushvidh <ayushvidhale099@gmail.com>
2022-03-08 18:04:04 +0530

    for firebase

--- app/src/main/java/com/example/charitable/donor_two.kt
+++ app/src/main/java/com/example/charitable/donor_two.kt
@@ -2,15 +2,23 @@ package com.example.charitable
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import com.example.charitable.firebase.FirestoreClass
+import com.google.firebase.firestore.auth.User
 import kotlinx.android.synthetic.main.activity_donor_two.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class donor_two : BaseActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addFragment(HomeFragment_donor.newInstance())
         setContentView(R.layout.activity_donor_two)
+
+        FirestoreClass().loadUserData(this)
 
         donor_bottom_nav.add(MeowBottomNavigation.Model(0,R.drawable.ic_home_))
         donor_bottom_nav.add(MeowBottomNavigation.Model(1,R.drawable.ic_history))
@@ -27,22 +35,29 @@ class donor_two : BaseActivity() {
 
                 }
                 2 -> {
+
+//                    updateNavigationUserDetails(user = com.example.charitable.models.User())
                     replaceFragment(ProfileFragment_donor.newInstance())
                 }
-
-//
-//                3-> {
-//                    replaceFragment(ProfileFragment_donor.newInstance())
-
-             //   }
                 else -> {
                     replaceFragment(HomeFragment_donor.newInstance(),)
-
 
                 }
             }
         }
     }
+
+    fun updateNavigationUserDetails(user: com.example.charitable.models.User){
+        Glide
+            .with(this)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_person)
+            .into(profile_image);
+        profile_name.text = user.name
+
+    }
+
     private fun replaceFragment(fragment: Fragment) {
         val fragmentTransition = supportFragmentManager.beginTransaction()
         fragmentTransition.replace(R.id.FragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
