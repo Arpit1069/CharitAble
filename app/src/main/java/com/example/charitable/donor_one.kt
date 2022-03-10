982b4096120caa88795e5559fe7221ef5b0eee13
Pranamya-19 <pranu11v@gmail.com>
2022-03-10 23:49:21 +0530

    update 1003 2349

--- app/src/main/java/com/example/charitable/donor_one.kt
+++ app/src/main/java/com/example/charitable/donor_one.kt
@@ -1,35 +1,144 @@
 package com.example.charitable
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
 import android.util.Log
+import android.webkit.MimeTypeMap
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.MimeTypeFilter
+import com.bumptech.glide.Glide
 import com.example.charitable.firebase.FirestoreClass
 import com.example.charitable.models.User
 import com.example.charitable.utils.Constants
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_donor_one.*
 import kotlinx.android.synthetic.main.activity_selection.*
 import kotlinx.android.synthetic.main.login.*
+import java.io.IOException
+import java.lang.Exception
 
 class donor_one :  BaseActivity() {
 
     private lateinit var mUserDetails:User
+    private var mSelectedImageFileUri: Uri? = null
+    private var mProfileImageURL : String = ""
 
+    companion object{
+        private const val READ_STORAGE_PERMISSION_CODE = 11
+        private const val PICK_IMAGE_REQUEST_CODE = 12
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donor_one)
         FirestoreClass().loadUserData(this)
 
+        profileUpdateImageDonor.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+
+            }else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+
+
         btnsubmitdonor.setOnClickListener {
 
+            if(mSelectedImageFileUri != null){
+                uploadUserImage()
+            }
             val intent = Intent(this@donor_one,splash2::class.java)
             updateUserProfileDataDonor()
             startActivity(intent)
             finish()
         }
     }
+
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode== READ_STORAGE_PERMISSION_CODE){
+            if (grantResults.isNotEmpty()&& grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                Toast.makeText(
+                    this,
+                    "permission denied", Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+        private fun showImageChooser(){
+        var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode== PICK_IMAGE_REQUEST_CODE && data!!.data !=null){
+            mSelectedImageFileUri = data.data
+            try{
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_person)
+                    .into(profileUpdateImageDonor)}catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun uploadUserImage(){
+        showProgressDialog(resources.getString(R.string.please_wait))
+        if(mSelectedImageFileUri != null){
+            val sRef : StorageReference = FirebaseStorage.getInstance().reference.child("USER_IMAGE" +
+                    System.currentTimeMillis() + "." + getFileExtension(mSelectedImageFileUri))
+            sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
+                taskSnapshot ->
+                Log.e(
+                    "Firebase Image URL", taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+                )
+                taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+                    uri ->
+                    Log.i("Downloadable Image URl",uri.toString())
+                    mProfileImageURL = uri.toString()
+                    hideProgressDialog()
+                    //TODO update user profile data
+
+                }
+
+            }.addOnFailureListener{
+                exception ->
+                Toast.makeText(this@donor_one,exception.message,Toast.LENGTH_LONG).show()
+                hideProgressDialog()
+            }
+        }
+    }
+
+    private fun getFileExtension(uri: Uri?): String?{
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
+    }
+
 
     private fun updateUserProfileDataDonor(){
         val userHashMap = HashMap<String,Any>()
